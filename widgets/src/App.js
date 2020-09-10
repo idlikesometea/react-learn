f3057e825257017f8504280054d13af186d29e6f
idlikesometea <im@virket.com>
2020-09-09 22:19:06 -0500

    build accordion component

---
+++ widgets/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Accordion from './components/Accordion';
+
+const items = [{
+        title: 'What is React?',
+        content: 'React is a front end javascript library'
+    }, {
+        title: 'Why use React?',
+        content: 'React is awesome!'
+    }, {
+        title: 'How do you use React?',
+        content: 'You use React with components'
+    }
+];
+
+export default () => {
+    return (
+        <div className="ui container">
+            <Accordion items={items} />
+        </div>
+    );
+}
