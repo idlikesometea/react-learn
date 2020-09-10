8d4c67285860b13bf62bab02ce196a21212a40ec
idlikesometea <im@virket.com>
2020-09-09 20:37:41 -0500

    add styles and default term

--- videos/src/components/App.js
+++ videos/src/components/App.js
@@ -7,12 +7,19 @@ import youtube from '../api/youtube';
 class App extends React.Component {
     state = { videos: [], selectedVideo: null };
 
+    componentDidMount() {
+        this.onTermSubmit('Formula 1');
+    }
+
     onTermSubmit = async term => {
         const response = await youtube.get('/search', {
             params: { q:term }
         });
 
-        this.setState({videos: response.data.items});
+        this.setState({
+            videos: response.data.items,
+            selectedVideo: response.data.items[0]
+        });
     }
 
     onVideoSelect = video => {
@@ -23,8 +30,16 @@ class App extends React.Component {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit}/>
-                <VideoDetail video={this.state.selectedVideo} />
-                <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+                <div className="ui grid">
+                    <div className="ui row">
+                        <div className="eleven wide column">
+                            <VideoDetail video={this.state.selectedVideo} />
+                        </div>
+                        <div className="five wide column">
+                            <VideoList videos={this.state.videos} onVideoSelect={this.onVideoSelect} />
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
