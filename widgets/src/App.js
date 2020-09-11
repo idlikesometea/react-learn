8ca64ffd56c8cb6b5c884ee31384ae3404d566e4
idlikesometea <im@virket.com>
2020-09-10 19:51:29 -0500

    search widget

--- widgets/src/App.js
+++ widgets/src/App.js
@@ -1,22 +1,24 @@
 import React from 'react';
-import Accordion from './components/Accordion';
+// import Accordion from './components/Accordion';
+import Search from './components/Search';
 
-const items = [{
-        title: 'What is React?',
-        content: 'React is a front end javascript library'
-    }, {
-        title: 'Why use React?',
-        content: 'React is awesome!'
-    }, {
-        title: 'How do you use React?',
-        content: 'You use React with components'
-    }
-];
+// const items = [{
+//         title: 'What is React?',
+//         content: 'React is a front end javascript library'
+//     }, {
+//         title: 'Why use React?',
+//         content: 'React is awesome!'
+//     }, {
+//         title: 'How do you use React?',
+//         content: 'You use React with components'
+//     }
+// ];
 
 export default () => {
     return (
-        <div className="ui container">
-            <Accordion items={items} />
+        <div>
+            {/* <Accordion items={items} /> */}
+            <Search />
         </div>
     );
 }
