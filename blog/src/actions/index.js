27fc6faca7254ebc09228fc43df504c89e52076c
idlikesometea <im@virket.com>
2020-09-29 21:54:15 -0500

    refactor with _.chain

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -4,8 +4,14 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 export const fetchPostAndUsers = () => async (dispatch, getState) => {
     await dispatch(fetchPosts());
 
-    const userIds = _.uniq(_.map(getState().posts, 'userId'));
-    userIds.forEach(userId => dispatch(fetchUser(userId)));
+    // const userIds = _.uniq(_.map(getState().posts, 'userId'));
+    // userIds.forEach(userId => dispatch(fetchUser(userId)));
+
+    _.chain(getState().posts)
+        .map('userId')
+        .uniq()
+        .forEach(id => dispatch(fetchUser(id)))
+        .value()
 };
 
 export const fetchPosts = () => async dispatch => {
