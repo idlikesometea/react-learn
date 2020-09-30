cd1098b7206088ccfac6da5f502735a066980ed2
idlikesometea <im@virket.com>
2020-09-29 21:49:33 -0500

    fetch users and refactor with lodash

--- blog/src/components/PostList.js
+++ blog/src/components/PostList.js
@@ -1,26 +1,34 @@
 import React from 'react';
 import { connect } from 'react-redux';
-import { fetchPosts } from '../actions';
+import { fetchPostAndUsers } from '../actions';
+import UserHeader from './UserHeader';
 
 class PostList extends React.Component {
 
     renderPosts() {
-        return this.props.posts.map((post, index) => {
+        return this.props.posts.map(post => {
             return (
-                <div key={index}>
-                    <h2>{post.title}</h2>
-                    {post.body}
+                <div className="item" key={post.id}>
+                    <i className="large middle aligned icon user" />
+                    <div className="content">
+                        <div className="description">
+                            <h2>{post.title}</h2>
+                            <p>{post.body}</p>
+                        </div>
+                        <UserHeader userId={post.userId} />
+                    </div>
                 </div>
             )
         })
     }
 
     componentDidMount() {
-        this.props.fetchPosts();
+        this.props.fetchPostAndUsers();
+
     }
 
     render() {
-        return <div>{this.renderPosts()}</div>
+        return <div className="ui relaxed divided list">{this.renderPosts()}</div>
     }
 };
 
@@ -29,5 +37,5 @@ const mapPropsToState = state => {
 };
 
 export default connect(mapPropsToState, {
-    fetchPosts
+    fetchPostAndUsers
 })(PostList);
